89135426ab6d9c3c3b5ad754881fe1bbd64fbc95
Emmsii <matty.93@hotmail.com>
2017-12-02 16:31:50 +0000

    More work on turrets
    + Added bean bags
    + Other turret things

---
+++ LD40/Assets/Scripts/SetRandomColor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using UnityEngine;
+
+public class SetRandomColor : MonoBehaviour {
+
+    public Color[] colors;
+    
+	void Start () {
+        Material material = GetComponent<Renderer>().material;
+        System.Random random = new System.Random();
+        material.color = colors[random.Next(colors.Length - 1)];
+	}
+	
+}
