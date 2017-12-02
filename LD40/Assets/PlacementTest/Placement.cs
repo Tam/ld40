ca91495e793cdc4b2e072ffbe7b5c266a5447091
zeronutty <zeronutty@googlemail.com>
2017-12-02 11:16:56 +0000

    Placement Scripts
    Placement

--- LD40/Assets/PlacementTest/Placement.cs
+++ LD40/Assets/PlacementTest/Placement.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class Placement : MonoBehaviour
 {
+    public bool Placing = false;
 
+    public int TurretToPlaceID = 100;
+    public GameObject[] TurretHolo;
+    public GameObject[] TurretsToPlace;
+
+    Vector3 Default = new Vector3(0, -2, 0);
+    PlacementCollision PC;
 	// Use this for initialization
 	void Start ()
     {
@@ -17,21 +24,51 @@ public class Placement : MonoBehaviour
         //Go into Placement Mode.
         if (Input.GetKeyUp(KeyCode.T))
         {
-            //Start trying to place Object
-            if (Input.GetMouseButtonDown(0))
+             TurretToPlaceID = 0;
+        }
+
+        if (TurretToPlaceID != 100)
+        {
+            //Fire Raycast from Camera To Mouse Position
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit Hit = new RaycastHit();
+
+            Debug.DrawRay(ray.origin, ray.direction * 100, Color.green);
+
+            if (Physics.Raycast(ray, out Hit, 100))
             {
-                //Fire Raycast from Camera To Mouse Position
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit Hit = new RaycastHit();
+                Vector3 HitPos = new Vector3(Hit.point.x, 1, Hit.point.z);
 
-                Debug.DrawRay(ray.origin, ray.direction * 100, Color.green);
-
-
-                if (Physics.Raycast(ray, out Hit, 100))
+                if (PC == null)
                 {
-
+                    PC = TurretHolo[TurretToPlaceID].GetComponent<PlacementCollision>();
                 }
-            }
+
+                TurretHolo[TurretToPlaceID].transform.position = HitPos;
+
+                if(!PC.IsColliding)
+                {
+                    TurretHolo[TurretToPlaceID].GetComponent<Renderer>().material.color = Color.green;
+
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        TurretHolo[TurretToPlaceID].transform.position = Default;
+                        Instantiate(TurretsToPlace[TurretToPlaceID], HitPos, Quaternion.identity);
+                        TurretToPlaceID = 100;                      
+                    }
+                }
+                else
+                {
+                    TurretHolo[TurretToPlaceID].GetComponent<Renderer>().material.color = Color.red;
+
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        Debug.Log("Ermmm There is something in the way bro !!");
+                    }
+                }
+                //Start trying to place Object
+               
+            }        
         }
 	}
 }
