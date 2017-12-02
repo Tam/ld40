2127fcabdeb5ccbb61c63b5fa7c56cecd558d069
zeronutty <zeronutty@googlemail.com>
2017-12-02 09:49:05 +0000

    Global Vars
    Global Vars

--- LD40/Assets/Scripts/GlobalVars.cs
+++ LD40/Assets/Scripts/GlobalVars.cs
@@ -2,15 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GlobalVars : MonoBehaviour {
+public class GlobalVars : MonoBehaviour
+{
+    //Time Playing the game from start to finish.
+    public float TimeElapsed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //--------------------------------------------AI------------------------------------------------//
+
+    //Amount of globflops on playing felid
+    public int AmountGlobFlops;
+
+    //Maxuim Amount of GlobFlops Allowed
+    public int MaxAmountGlobFlops;
+
+    //Minium Amount of GlobFlops Allowed
+    public int MinAmountGlobFlops;
+
+    //Amount of GlobFlops killed
+    public int AmountGlobFlopsKilled;
+
+    //Amount of Protestors
+    public int AmountProtestors;
+
+    //Maxuim Amount Of Protestors
+    public int MaxAmountProtestor;
+
+    //------------------------------------------Mechcanics------------------------------------------//
+
+    //Amount of Currency
+    public float AmountCurrency;
+
+    //Global Price SuperValibleUnObtainium
+    public float GobalPriceUnobtainium;
+
+    //------------------------------------------Placements------------------------------------------//
+
+    //Amount of Turrents Placed
+    public int TurretsPlaced;
+
+    //Amount of Traps Placed
+    public int TrapsPlaced;
+
+
+    
+
 }
