37daa5929bed1d7b67d1cf564cc8d6e3b588ff88
Emmsii <matty.93@hotmail.com>
2017-12-02 12:40:52 +0000

    More work to turrets
    + Turrets can look at things

---
+++ LD40/Assets/Scripts/BulletController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // GAME OBJECT SHOULD BE AN ENEMY AI SCRIPT
+    // Or use GetComponent<EnemyAI> on enemy
+    public void OnHit(GameObject enemy)
+    {
+
+    }
+}
--- LD40/Assets/Scripts/TurretController.cs
+++ LD40/Assets/Scripts/TurretController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class TurretController : MonoBehaviour {
 
+    public enum FireType
+    {
+        Projectile,
+        Particle,
+        Line_Renderer
+    }
+
     private GameObject target;
 
     [Header("Turret Parts")]
@@ -13,26 +20,31 @@ public class TurretController : MonoBehaviour {
     [Header("General")]
     public string enemyTag;
     public float range = 10f;
+    public float turnSpeed = 2.5f;
+    public float findTargetFrequency = 0.75f;
 
-    [Header("Use Projectiles")]
-    public bool useProjectiles;
+    public FireType fireType;
+
+    [Header("Projectile Properties")]
     public GameObject bulletPrefab;
     public float fireRate = 1f;
-    
-    [Header("Use Particles")]
-    public bool useParticles;
 
-    [Header("Use Line Renderer")]
-    public bool useLineRenderer;
+    [Header("Particle Properties")]
+
+    [Header("Line Renderer Properties")]
+    public LineRenderer lineRenderer;
+    // TODO: Other stats
 
     [Header("Debug")]
     public Color radiusColor = Color.red;
-    public bool showRadius;
-    public bool showRayToTarget;
+    private Color targetLineColor = Color.red;
+    public bool showRadius = true;
+    public bool showRayToTarget = true;
 
 	// Use this for initialization
-	void Start () {
-        InvokeRepeating("FindTarget", 1, 1);
+	void Start ()
+    {
+        InvokeRepeating("FindTarget", 0, findTargetFrequency);
 	}
 	
     void FindTarget()
@@ -55,9 +67,98 @@ public class TurretController : MonoBehaviour {
     }
 
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		if(target != null)
+        {
+            TrackTarget();
+
+            Shoot();
+        }
+        else
+        {
+            if(lineRenderer != null)
+            {
+                lineRenderer.enabled = false;
+            }
+        }
 	}
+
+    void Shoot()
+    {
+        switch (fireType)
+        {
+            case FireType.Projectile:
+                ShootProjectile();
+                break;
+            case FireType.Particle:
+                ShootParticle();
+                break;
+            case FireType.Line_Renderer:
+                ShootLineRenderer();
+                break;
+            default:
+                Debug.LogError("[WUT] Unknown fire type.");
+                break;
+        }
+    }
+
+    void ShootProjectile()
+    {
+
+    }
+
+    void ShootParticle()
+    {
+
+    }
+
+    void ShootLineRenderer()
+    {
+        if (target == null) return;
+
+        if(lineRenderer == null)
+        {
+            throw new UnityException("Did not define a line renderer for turret.");
+        }
+
+        IsLookingAtTarget();
+
+        //lineRenderer.enabled = true;
+
+        lineRenderer.SetPosition(0, muzzlePoint.position);
+        lineRenderer.SetPosition(1, target.transform.position);
+    }
+
+    bool IsLookingAtTarget()
+    {
+        targetLineColor = Color.red;
+        if (target == null) return false;
+        Debug.Log("RAY");
+
+        RaycastHit hit;
+        Debug.DrawRay(muzzlePoint.transform.position, muzzlePoint.transform.TransformDirection(Vector3.forward) * 100f, Color.cyan);
+        if(Physics.Raycast(muzzlePoint.transform.position, muzzlePoint.transform.TransformDirection(Vector3.forward), out hit, 100f))
+        {
+            Debug.Log(hit.transform.gameObject.GetInstanceID() + " + " + target.GetInstanceID());
+            if (hit.transform.gameObject.GetInstanceID() == target.GetInstanceID())
+            {
+                Debug.Log("Is looking at target");
+                targetLineColor = Color.blue;
+                return true;
+            }
+        }
+        targetLineColor = new Color(255, 157, 10);
+        return false;
+    }
+
+    void TrackTarget()
+    {
+        Vector3 dir = target.transform.position - turretHead.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(turretHead.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        turretHead.rotation = Quaternion.Euler(rotation.x, rotation.y, rotation.z);
+    }
 
     void OnDrawGizmos()
     {
@@ -70,14 +171,15 @@ public class TurretController : MonoBehaviour {
         if(target != null && showRayToTarget)
         {
             //TODO: Set color to green is target in range, else red
-            if (Vector3.Distance(transform.position, target.transform.position) <= range)
-            {
-                Gizmos.color = Color.blue;
-            }
-            else
-            {
-                Gizmos.color = Color.red;
-            }
+            //if (Vector3.Distance(transform.position, target.transform.position) <= range)
+            //{
+            //    Gizmos.color = Color.blue;
+            //}
+            //else
+            //{
+            //    Gizmos.color = Color.red;
+            //}
+            Gizmos.color = targetLineColor;
             Gizmos.DrawLine(muzzlePoint.transform.position, target.transform.position);
         }
     }
