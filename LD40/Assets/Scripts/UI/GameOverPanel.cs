130050a75dccb5d882c7fb28aef25e989a9cd6af
Tam <hi@tam.sx>
2017-12-03 12:54:00 +0000

    Working on UI

---
+++ LD40/Assets/Scripts/UI/GameOverPanel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+	public class GameOverPanel : BasePanel
+	{
+		
+		// Variables
+		// =====================================================================
+
+		public Text scoreText;
+		
+		// Events
+		// =====================================================================
+
+		public void OnMainMenuClick()
+		{
+			Debug.Log("MAIN MENU");
+		}
+
+		public void OnRestartClick()
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+
+	}
+}
--- LD40/Assets/Scripts/UI/UIManager.cs
+++ LD40/Assets/Scripts/UI/UIManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
@@ -9,7 +10,49 @@ namespace UI
 		// Variables
 		// =====================================================================
 
+		public GlobalVars globalVars;
+
+		public Button overlayButton;
+		
 		public UpgradePanel upgradePanel;
+
+		public GameOverPanel gameOverPanel;
+		
+		// Unity
+		// =====================================================================
+
+		private void Awake()
+		{
+			// Make sure everything is hidden
+			upgradePanel.Hide();
+			gameOverPanel.Hide();
+			
+			// TODO: When opening a panel w/ overlay, add panel to overlay list
+			// TODO: When closing panel, remove from overlay list (hide overlay if list now empty)
+			// TODO: On overlay click close all panels in overlay list
+		}
+
+		// Actions
+		// =====================================================================
+
+		public void ShowOverlay()
+		{
+			overlayButton.gameObject.SetActive(true);
+		}
+
+		public void HideOverlay()
+		{
+			overlayButton.gameObject.SetActive(false);
+		}
+
+		public void ShowGameOver(float score)
+		{
+			gameOverPanel.scoreText.text = score + "";
+			gameOverPanel.Show();
+			
+			// Pause the game
+			globalVars.Pause();
+		}
 
 	}
 }
